c561cc248959f3580d38f59b6fb7b21a5c9f40bc
Collins Ezedike-egwom <ezedikecollins@gmail.com>
2025-07-08 20:13:45 +0100

    testing application

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,39 @@ mod order;
 mod store;
 mod product;
 
+use store::*;
+use order::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut store: Store = Store::new();
+
+    let products_to_add = [
+        ("MacBook Pro", 1999.99, 8),
+        ("AirPods", 199.99, 3),
+        ("iPhone 14", 899.99, 0),
+    ];
+
+    for product in products_to_add {
+        store.add_product(product.0, product.1, product.2);
+    }
+
+    store.list_products();
+
+    let orders_to_place = [
+        ("Alice Johnson", 1, 2, "Shipped"),
+        ("Bob Smith", 2, 5, "Cancelled"),
+        ("Charlie Daniels", 3, 1, "Pending"),
+        ("James Carnage", 102, 1, "Pending"),
+    ];
+
+    for order in orders_to_place {
+        store.place_order(order.0, order.1, order.2);
+    }
+    store.list_orders();
+
+    store.update_order_status(101, OrderStatus::Shipped);
+    store.list_orders();
+
+    store.update_order_status(101, OrderStatus::Delivered);
+    store.list_orders();
 }
